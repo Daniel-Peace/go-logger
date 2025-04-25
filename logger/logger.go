7a41a9bde23febd262355359ac0e5829e239457a
Daniel Peace <danielpeace805@gmail.com>
2025-04-24 20:49:07 -0700

    added print printf and println functionality to the logger

--- logger/logger.go
+++ logger/logger.go
@@ -6,27 +6,85 @@ import (
 )
 
 type GoLogger struct {
-	OutputSteam  io.Writer
-	DateAndTime  bool
-	FunctionName bool
+	outputSteam  io.Writer
+	dateAndTime  bool
+	fileName     bool
+	functionName bool
+	lineNumber   bool
 }
 
-func NewGoLogger() {
+var buffer []byte
 
+// func appendToBuffer(v ...any) {
+// 	buffer = append(buffer, v...)
+// }
+
+func NewGoLogger(outputStream io.Writer, dateAndTime bool, fileName bool, functionName bool, lineNumber bool) *GoLogger {
+	return &GoLogger{
+		outputSteam:  outputStream,
+		dateAndTime:  dateAndTime,
+		fileName:     fileName,
+		functionName: functionName,
+		lineNumber:   lineNumber,
+	}
+}
+
+func (l *GoLogger) generateLog(buffer *[]byte) {
+	l.formatHeader(buffer)
+}
+
+func (l *GoLogger) formatHeader(buffer *[]byte) {
+	showDateAndTime := l.dateAndTime
+	showFileName := l.fileName
+	showFunctionName := l.functionName
+	showLineNumber := l.lineNumber
+
+	if showDateAndTime {
+		*buffer = append(*buffer, "[date and time]"...)
+	}
+
+	if showFileName {
+		*buffer = append(*buffer, "[file name]"...)
+	}
+
+	if showFunctionName {
+		*buffer = append(*buffer, "[function name]"...)
+	}
+
+	if showLineNumber {
+		*buffer = append(*buffer, "[line number]"...)
+	}
 }
 
 func (l *GoLogger) Print(a ...any) {
-	fmt.Print(a...)
+	var buffer []byte
+	l.formatHeader(&buffer)
+	fmt.Append(buffer, a...)
+	buffer = fmt.Append(buffer, a...)
+	l.WriteToStream(&buffer)
 }
 
-func (l *GoLogger) Printf() {
-
+func (l *GoLogger) Printf(format string, a ...any) {
+	var buffer []byte
+	l.formatHeader(&buffer)
+	fmt.Append(buffer, a...)
+	buffer = fmt.Appendf(buffer, format, a...)
+	l.WriteToStream(&buffer)
 }
 
-func (l *GoLogger) Println() {
-
+func (l *GoLogger) Println(a ...any) {
+	var buffer []byte
+	l.formatHeader(&buffer)
+	fmt.Append(buffer, a...)
+	buffer = fmt.Appendln(buffer, a...)
+	l.WriteToStream(&buffer)
 }
 
+// Takes a buffer and writes to the specified output stream.
+// It applies a newline to the end of the buffer if one does not already exist
 func (l *GoLogger) WriteToStream(buffer *[]byte) {
-	l.OutputSteam.Write(*buffer)
+	if (*buffer)[len(*buffer)-1] != '\n' {
+		*buffer = append(*buffer, '\n')
+	}
+	l.outputSteam.Write(*buffer)
 }
